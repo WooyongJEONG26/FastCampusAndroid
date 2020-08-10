c60f3af142df737c81b81696168c91d07418606b
WooyongJeong <dndyd0206@gmail.com>
2020-08-10 06:04:51 -0400

    Final Assignment T

--- app/src/main/java/org/techtown/myapplication/MainActivity.kt
+++ app/src/main/java/org/techtown/myapplication/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.final_assignment)
+        setContentView(R.layout.final_assignment_t)
     }
 }
