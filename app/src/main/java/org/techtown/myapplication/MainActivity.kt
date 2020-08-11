6179b83309f172367c19c62c22eec1126d52087a
WooyongJeong <dndyd0206@gmail.com>
2020-08-11 07:12:46 -0400

    20 0811 Creating drawable file

--- app/src/main/java/org/techtown/myapplication/MainActivity.kt
+++ app/src/main/java/org/techtown/myapplication/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.final_assignment_t)
+        setContentView(R.layout.final_assignment)
     }
 }
