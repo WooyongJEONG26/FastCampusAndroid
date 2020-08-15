e89d5ace78a21e324add87aecb1686586eb921de
WooyongJeong <dndyd0206@gmail.com>
2020-08-15 09:57:09 -0400

    20 0815 Calculator practice

---
+++ app/src/main/java/org/techtown/myapplication/CalcualtorT.kt
@@ -0,0 +1,12 @@
+package org.techtown.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalcualtorT : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calcualtor_t)
+    }
+}
